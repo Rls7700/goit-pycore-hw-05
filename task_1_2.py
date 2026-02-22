088e7d448c992b16eabbb04880094e14c8f4f9bf
Rls7700 <rlstr@protonmail.com>
2026-02-22 20:20:40 +0200

    Update homework tasks

--- task_1_2.py
+++ task_1_2.py
@@ -1,33 +1,24 @@
-# ЗаЗАВДАННЯ 1
-# замикання + кеш + рекурсія
-# caching_fibonacci
+# ЗАВДАННЯ 1
 
 from typing import Callable, Generator
 import re
 
+
 def caching_fibonacci() -> Callable[[int], int]:
-    """
-    функція створює кеш та повертає внутрішню функцію
-    fibonacciб яка памяає кеш
-    """
-    cache = {} # словник для збереження обчислених значень
+    """ф-я створює кеш та повертає внутрішню функцію fibonacciб яка памяає кеш"""
+    cache = {} 
+
 
     def fibonacci(n: int) -> int:
-        """
-        обчислює n-e число рекурсивно з використанням кешу
-        """
-        # базові випадки (для зупинки рекурсії)
         if n <=0:
             return 0
-        if n==1:
+        if n == 1:
             return 1
         
-        # якщо прораховували - повертаємо з кеш
         if n in cache:
             return cache[n]
         
-        # якщо не рахували - обчислюємо
-        cache[n] = fibonacci(n-1) + fibonacci(n-2)
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
         return cache[n]
 
     return fibonacci
@@ -36,20 +27,18 @@ def caching_fibonacci() -> Callable[[int], int]:
 # ЗАВДАННЯ 2
 
 def generator_numbers(text: str) -> Generator[float, None, None]:
-    """
-    генератор знаходить дійсні числа
-    відокремлені пробілами з обох боків
-    """
+    """генератор знаходить дійсні числа відокремлені пробілами з обох боків"""
+
     pattern = r" (?P<num>\d+\.\d+) "
-    # finditer дає всі збіги по тексту 
+
     for match in re.finditer(pattern, text):
         yield float(match.group("num"))
 
-def sum_profit(text: str, func: Callable[[str], Generator[float, None, None]]) -> float:
-    """
-    func - функція генератор
-    sum(...) пыдсумовує значення які видасть генератор
-    """
+
+def sum_profit(
+    text: str, 
+    func: Callable[[str], 
+    Generator[float, None, None]]) -> float:
     return sum(func(text))
 
 # Запуск
@@ -58,7 +47,6 @@ if __name__ =="__main__":
     print("= ЗАВДАННЯ 1 =")
 
     fib = caching_fibonacci()
-
     print("fib(10):", fib(10)) #55
     print("fib(15):", fib(15)) #610
 
@@ -67,10 +55,10 @@ if __name__ =="__main__":
     text =(
         "Загальний дохід працівника складається з декількох частин: "
         "1000.01 як основний дохід, доповнений додатковими надхлдженнями"
-        " 27.45 і 324.00 доларів. Test space-500.00 "
+        " 27.45 і 324.00 доларів."
     )
 
     total_income = sum_profit(text, generator_numbers)
-    print("загальний дохід:", total_income)
+    print("Загальний дохід:", total_income)
     
     
--- task_4.py
+++ task_4.py
@@ -1,124 +1,86 @@
 from typing import Dict, List, Tuple, Callable
 
 def input_error(func: Callable):
-    """
-    деоратор для обробки помилок вводу від користувача
-    обгортаємо" ф-ю (add_contact)
-    якщо в середині помилка то програма не завершується
-    а повертає такст повідомлення
-    """
+    """Декоратор для обробки помилок вводу від користувача"""
+
     def inner(*args, **kwargs):
         try:
-
-            # пробуємо виконати загорнуту ф-ю
             return func(*args,**kwargs)
         
         except IndexError:
-            # команда phone без імені ->викликає IndexError
             return "Enter the argument for the command"
         
         except ValueError:
-            # імя без тел -> name,phone = args дасть ValueError
             return "Give me name and phone please"
         
         except KeyError:
-            #наприклад запитали тел контакта якого немає
             return "Contact not found"
         
     return inner
 
-def parse_input(user_input: str) -> Tuple[str, List[str]]:
-    """
-    Розбирає введення користувача на:
-    команду
-    список аргументів
-    """
-    parts = user_input.strip().split()
 
-    # якщо натиснув просто Enter -> parts буде порожній
+def parse_input(user_input: str) -> Tuple[str, List[str]]:
+    """Розбирає введення користувача"""
+    parts = user_input.strip().split()
     if not parts:
         return "", []
     
-    # перше слово -це команда
     command = parts[0].lower()
-
-    # все інше аргументи
     args = parts[1:]
-
     return command, args
+
 
 @input_error
 def add_contact(args: List[str], contacts: Dict[str, str]) -> str:
-    """
-    додає новий контакт
-    очікує args = [name, phone]
-    """
+    """Додає новий контакт очікує args = [name, phone]"""
     name, phone = args
-
-    # зберігаємо в словник ключ = імя, значення = тел
     contacts[name] = phone
     return "Contact added"
 
+
 @input_error
 def change_contact(args: List[str], contacts: Dict[str,str]) -> str:
-    """
-    оновлює тел існуючого контакту
-    """
+    """Оновлює тел. існуючого контакту"""
     name, phone = args
-
-    # якщо контакта немає -  KeyError
-    # щоб декоратор повернув "Contact not found"
     if name not in contacts:
         raise KeyError
+    
     contacts[name] = phone
     return "Contact updated"
 
+
 @input_error
 def show_phone(args: List[str], contacts: Dict[str, str]) -> str:
-    """
-    показує тел контакта
-    очікує args = [name]
-    """
+    """Показує тел контакта очікує args = [name]"""
     name = args[0]
     if name not in contacts:
         raise KeyError
+    
     return f"{name}: {contacts[name]}"
+
 
 @input_error
 def show_all(contacts: Dict[str, str]) -> str:
-    """
-    показує всі контакти
-    """
+    """Показує всі контакти"""
     if not contacts:
         return "No contacts yet"
     
-    # формує список рядків "імя: телефон"
     lines = [f"{name}: {phone}" for name, phone in contacts.items()]
-
-    # обєднуємо рядки через перенос строки
     return "\n" .join(lines)
 
-def main() -> None:
-    """
-    готовий цикл бота
-    програма чекає команди користувача
-    """
 
+def main() -> None:
+    """Готовий цикл бота"""
     contacts: Dict[str, str] = {}
     print("Welcome to the assistant bot!")
 
     while True:
-        # просимо ввод команди
         user_input = input("Enter a command: ")
-
-        # розбираємо команду та аргументи
         command, args = parse_input(user_input)
 
-        # команди виходу
         if command in ("exit", "close"):
             print("Good bye!")
             
-
         elif command == "Hello":
             print("How can i help you?")
 
@@ -140,6 +102,7 @@ def main() -> None:
 
         else:
             print("Invalid command")
+            
 
 if __name__ == "__main__":
     main() 
